784c500b9cea4b2fb707fa56c44eb2663dc99479
BlankoDev <169070307+BlankoDev@users.noreply.github.com>
2025-08-14 04:23:29 +0200

    Delete mistake

--- class_utils.py
+++
@@ -1,7 +0,0 @@
-"""
-A module full (not for now) of useful helper class, used in many projects by Blanko
-"""
-
-class HierarchizedObject:
-    def __init__(self, parent):
-        self._parent = parent
